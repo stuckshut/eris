c0a61b0e93d9b48ae31d60d4b7d7e894e50cfc3c
Eric <eric@stuckshut.com>
2016-01-17 06:39:19 -0600

    Add tests for commands, fix tests for eris

--- tests/test_eris.py
+++ tests/test_eris.py
@@ -1,112 +1,76 @@
 from unittest import TestCase
-from unittest.mock import MagicMock
+from unittest.mock import patch, MagicMock
 
 import eris
-
 
 class ErisTest(TestCase):
     """Tests the eris.py module"""
 
     def setUp(self):
+        self.message = MagicMock()
+        self.message.author.id = 10
         eris.client = MagicMock()
+        eris.client.user.id = 20
 
-    def tearDown(self):
-        pass
-
+    @patch("eris.print", MagicMock())
     def test_on_ready(self):
         # Nothing really happens here...
-        eris.print = MagicMock()
         eris.on_ready()
         self.assertTrue(eris.print.called)
 
+    @patch("eris.commands.get_command_from_list", MagicMock())
     def test_on_message_self_message(self):
-        # Test setup items
-        msg = MagicMock()
-        eris.commands.get_command_from_list = MagicMock()
+        self.message.author.id = 20
 
-        # self_sent_message setup
-        msg.author.id = 10
-        eris.client.user.id = 10
-
-        eris.on_message(msg)
+        eris.on_message(self.message)
         eris.commands.get_command_from_list.assert_not_called()
 
+    @patch("eris.client.send_message", MagicMock())
+    @patch("eris.commands.get_command_from_list", MagicMock(return_value=None))
     def test_on_message_wrong_command(self):
-        # Setup items
-        msg = MagicMock()
-        eris.client.send_message = MagicMock()
-
-        # No command found setup
-        eris.commands.get_command_from_list = MagicMock(return_value=None)
-
-        eris.on_message(msg)
-        eris.commands.get_command_from_list.assert_called_with(msg)
+        eris.on_message(self.message)
+        eris.commands.get_command_from_list.assert_called_with(self.message)
         eris.client.send_message.assert_not_called()
 
+    @patch("eris.commands.get_command_from_list", MagicMock(return_value=MagicMock(channel_required=True)))
+    @patch("eris.client.send_message", MagicMock())
+    @patch("eris.is_direct_message", MagicMock(return_value=True))
     def test_on_message_channel_required(self):
-        # Test setup items
-        msg = MagicMock()
-        command = MagicMock()
-        eris.commands.get_command_from_list = MagicMock(return_value=command)
-        eris.client.send_message = MagicMock()
+        eris.on_message(self.message)
+        eris.client.send_message.assert_called_with(self.message.channel, eris.settings.CHANNEL_REQUIRED_MSG)
 
-        # Channel_required setups
-        command.channel_required = True
-        eris.is_direct_message = MagicMock(return_value=True)
-
-        eris.on_message(msg)
-        eris.client.send_message.assert_called_with(msg.channel, eris.settings.CHANNEL_REQUIRED_MSG)
-
+    @patch("eris.commands.get_command_from_list", MagicMock(return_value=MagicMock(admin_required=True)))
+    @patch("eris.client.send_message", MagicMock())
+    @patch("eris.message_is_from_admin", MagicMock(return_value=False))
     def test_on_message_admin_required(self):
-        # Test setup items
-        msg = MagicMock()
-        command = MagicMock()
-        eris.commands.get_command_from_list = MagicMock(return_value=command)
-        eris.client.send_message = MagicMock()
+        eris.on_message(self.message)
+        eris.client.send_message.assert_called_with(self.message.channel, eris.settings.UNAUTHORIZED_MSG)
 
-        # admin_required setups
-        command.admin_required = True
-        eris.message_is_from_admin = MagicMock(return_value=False)
-
-        eris.on_message(msg)
-        eris.client.send_message.assert_called_with(msg.channel, eris.settings.UNAUTHORIZED_MSG)
-
+    @patch("eris.message_is_from_admin", MagicMock(return_value=True))
+    @patch("eris.is_direct_message", MagicMock(return_value=False))
+    @patch("eris.commands.HelpCommand.execute", MagicMock())
+    @patch("eris.client.send_message", MagicMock())
     def test_on_message_execute_message(self):
-        # Test setup items
-        msg = MagicMock()
-        command_classes = MagicMock()
-        command = command_classes[0].return_value
-        command.execute = MagicMock(name="method")
-        command.execute.return_value = MagicMock()
-        eris.commands.get_command_from_list = MagicMock(return_value=command_classes)
-        eris.client.send_message = MagicMock()
+        self.message.content = "!help"
 
-        # command requirement setups
-        command.channel_required = False
-        command.admin_required = False
-        eris.is_direct_message = MagicMock(return_value=False)
-        eris.message_is_from_admin = MagicMock(return_value=True)
-
-        eris.on_message(msg)
-        command.execute.assert_called_once_with()
-        eris.client.send_message.assert_called_once_with(msg.channel, command.execute.return_value)
+        eris.on_message(self.message)
+        eris.client.send_message.assert_called_once_with(self.message.channel, eris.commands.HelpCommand.execute())
 
     def test_message_is_from_admin(self):
-        message = MagicMock()
         role = MagicMock()
         role.name = eris.settings.ADMIN_ROLE
 
-        message.author.roles = [role]
-        self.assertTrue(eris.message_is_from_admin(message))
+        self.message.author.roles = [role]
+        self.assertTrue(eris.message_is_from_admin(self.message))
 
-        message.author.roles = []
-        self.assertFalse(eris.message_is_from_admin(message))
+        self.message.author.roles = []
+        self.assertFalse(eris.message_is_from_admin(self.message))
 
     def test_message_is_direct_message(self):
-        message = MagicMock()
+        self.message = MagicMock()
 
-        message.author = MagicMock(eris.discord.User)
-        self.assertTrue(eris.is_direct_message(message))
+        self.message.channel = MagicMock(eris.discord.Channel)
+        self.assertFalse(eris.is_direct_message(self.message))
 
-        message.author = False
-        self.assertFalse(eris.is_direct_message(message))
+        self.message.channel = MagicMock(eris.discord.PrivateChannel)
+        self.assertTrue(eris.is_direct_message(self.message))
